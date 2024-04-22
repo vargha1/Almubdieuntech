5cc1f76dec7f0e92958c3d4c9e273e1c8ef38e89
vargha1 <vargha.rahavi@gmail.com>
2024-04-22 14:06:07 +0330

    VR36

--- script.js
+++ script.js
@@ -47,3 +47,81 @@ function preventScroll(e) {
 
     return false;
 }
+
+let wrapper1 = document.getElementById("wrapper1");
+var wrapper3 = document.getElementById('wrapper3');
+var wrapper4 = document.getElementById('wrapper4');
+
+if (window.innerWidth > 768) {
+    wrapper1.children.item(1).classList.add("hidden");
+    wrapper1.children.item(0).classList.remove("hidden");
+} else if (window.innerWidth < 768) {
+    wrapper1.children.item(0).classList.add("hidden");
+    wrapper1.children.item(1).classList.remove("hidden");
+}
+
+window.addEventListener('resize', () => {
+
+    if (window.innerWidth > 768) {
+        wrapper1.children.item(1).classList.add("hidden");
+        wrapper1.children.item(0).classList.remove("hidden");
+    } else if (window.innerWidth < 768) {
+        wrapper1.children.item(0).classList.add("hidden");
+        wrapper1.children.item(1).classList.remove("hidden");
+    }
+})
+
+function changeList(num) {
+    let wrapper2 = document.getElementById("wrapper2");
+
+    if (window.innerWidth > 768) {
+        for (let index = 0; index < wrapper3.children.length; index++) {
+            if (!(wrapper3.children.item(index).children.item(0).classList.contains('hidden'))) {
+                wrapper3.children.item(index).children.item(0).classList.add('hidden');
+            }
+            wrapper2.children.item(index).classList.remove("main-color");
+        }
+        wrapper3.children.item(num).children.item(0).classList.remove('hidden');
+        wrapper2.children.item(num + 1).classList.add("main-color");
+    }
+    if (window.innerWidth < 768) {
+        for (let index = 2; index < wrapper4.children.length + 1; index++) {
+            if (index % 2 === 0) {
+                wrapper4.children.item(index).children.item(0).classList.add("max-h-0");
+                wrapper4.children.item(index).children.item(0).classList.remove("max-h-[1000px]");
+
+            }
+            wrapper4.children.item(num).children.item(0).classList.remove("max-h-0");
+            wrapper4.children.item(num).children.item(0).classList.add("max-h-[1000px]");
+
+        }
+    }
+}
+
+class ListItem extends HTMLElement {
+    constructor() {
+        super();
+        let divElem = document.createElement("div");
+        this.append(Object.assign(divElem, { className: 'flex flex-col ps-8 border-s-[1px] overflow-hidden transition-all duration-500 transform-gpu' }));
+        divElem.innerHTML += `<h6 class="text-[20px] text-black font-[500]">${this.getAttribute("data-value1")}</h6>`
+        divElem.innerHTML += `<h4 class="text-[20px] font-bold mt-6">${this.getAttribute("data-value2")}</h4>`
+        divElem.innerHTML += `<p class="text-[14px] mt-4">${this.getAttribute("data-value3")}</p>`
+        divElem.innerHTML += `<h4 class="text-[20px] font-bold mt-6">${this.getAttribute("data-value4")}</h4>`
+        divElem.innerHTML += `<p class="text-[14px] mt-4">${this.getAttribute("data-value5")}</p>`
+        divElem.innerHTML += `<h4 class="text-[20px] font-bold mt-6">${this.getAttribute("data-value6")}</h4>`
+        divElem.innerHTML += `<p class="text-[14px] mt-4">${this.getAttribute("data-value7")}</p>`
+        divElem.innerHTML += `<h4 class="text-[20px] font-bold mt-6">${this.getAttribute("data-value8")}</h4>`
+        divElem.innerHTML += `<p class="text-[14px] mt-4">${this.getAttribute("data-value9")}</p>`
+    }
+}
+
+customElements.define('list-item', ListItem);
+
+wrapper3.children.item(0).children.item(0).classList.remove('hidden');
+wrapper4.children.item(2).children.item(0).classList.remove('hidden');
+
+if (window.innerWidth > 768) {
+    changeList(0);
+} else if (window.innerWidth < 768) {
+    changeList(2);
+}
